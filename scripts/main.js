52ae7cd345e4067cf27132869bc035b6d01019aa
Ground-Defence <58024423+Ground-Defence@users.noreply.github.com>
2023-04-13 20:38:26 +0100

    test upload for learning

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+/*
+Everything in between is a comment
+*/
+// single line comment
+
+
+// Image Switcher code
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/Wolf.jpg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+// Personalized welcome message code
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if(!myName){
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.innerHTML = 'Mozilla is cool, '+ myName;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.innerHTML = 'Mozilla is cool, '+ storedName;
+}
+
+myButton.onclick =() => {
+  setUserName();
+};
